c90f6228aa17faf0b87b19fab506de160e269038
Peter <chuy1483@gmail.com>
2022-08-29 02:18:40 -0400

    Added user BLE & Location permissions.

--- src/main/java/com/peter/bleapp/MainActivity.kt
+++ src/main/java/com/peter/bleapp/MainActivity.kt
@@ -1,17 +1,71 @@
 package com.peter.bleapp
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.peter.bleapp.databinding.ActivityMainBinding
+
+private const val ENABLE_BLUETOOTH_REQUEST_CODE = 1
+private const val LOCATION_PERMISSION_REQUEST_CODE = 2
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    //Bluetooth adapter variable
+    private val bluetoothAdapter: BluetoothAdapter by lazy {
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!bluetoothAdapter.isEnabled) {
+            promptEnableBluetooth() // It crashes here
+        }
+    }
+    //Bluetooth breaks emulator use physical device.
+    @SuppressLint("MissingPermission")
+    private fun promptEnableBluetooth() {
+
+        if (!bluetoothAdapter.isEnabled) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, ENABLE_BLUETOOTH_REQUEST_CODE)
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            ENABLE_BLUETOOTH_REQUEST_CODE -> {
+                if (resultCode != Activity.RESULT_OK) {
+                    promptEnableBluetooth()
+                }
+            }
+        }
+    }
+
+
+    private val isLocationPermissionGranted
+        get() = hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+
+    fun Context.hasPermission(permissionType: String): Boolean {
+        return ContextCompat.checkSelfPermission(this, permissionType) ==
+                PackageManager.PERMISSION_GRANTED
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- src/main/java/com/peter/bleapp/ui/connect/ConnectFragment.kt
+++ src/main/java/com/peter/bleapp/ui/connect/ConnectFragment.kt
@@ -1,22 +1,41 @@
 package com.peter.bleapp.ui.connect
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.peter.bleapp.LocationPermissionFragment
 import com.peter.bleapp.databinding.FragmentConnectBinding
 
 class ConnectFragment : Fragment() {
 
     private var _binding: FragmentConnectBinding? = null
+    private fun hasLocationPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+    private fun requestLocationPermission() {
+        val locationAlert = LocationPermissionFragment()
+        locationAlert.show(requireFragmentManager(), "locationAlertDialog")
+    }
+    private fun startBleScan() {
+        if(!hasLocationPermission()) {
+            requestLocationPermission()
+        } else {
+            //TODO implement stuff
+        }
+    }
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -28,6 +47,7 @@ class ConnectFragment : Fragment() {
         _binding = FragmentConnectBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        binding.buttonScan.setOnClickListener { startBleScan()  }
         binding.buttonConnect.setOnClickListener {
             binding.buttonConnect.text = "Connect2"
         }
